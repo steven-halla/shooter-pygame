688e14a1f202971d6b9d184992d55d2a660d6507
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-24 17:21:18 -0800

    "refactoring, creating soldier class"

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
 
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/player/Idle/0.png')
+        # lets us scale our image
+        self.img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        # draws rect around imgage
+        rect = img.get_rect()
+        rect.center = (x, y)
+
+
 #draws player coordinates
 x = 200
 y = 200
 scale = 3
-img = pygame.image.load('img/player/Idle/0.png')
-#lets us scale our image
-img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-#draws rect around imgage
-rect = img.get_rect()
-rect.center = (x, y)
+
 
 
 
