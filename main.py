a1fc857756941046c4609267a713d6eb4eb933c5
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-24 17:05:15 -0800

    "about to add images"

--- main.py
+++ main.py
@@ -8,14 +8,27 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
 
+#draws player coordinates
+x = 200
+y = 200
+img = pygame.image.load('img/player/Idle/0.png')
+#draws rect around imgage
+rect = img.get_rect()
+rect.center = (x, y)
+
+
 
 run = True
 while run:
+
+    screen.blit(img, rect)
 
     for event in pygame.event.get():
         #quit game
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 
 pygame.quit()
