8948017bd3834981881bf6a832e33e9711161701
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-24 20:20:57 -0800

    "can now flip player  image based on left or right key"

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ class Soldier(pygame.sprite.Sprite):
     def __init__(self, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
         self.speed = speed
-
+        self.direction = 1
+        self.flip = False #flips player imag
         img = pygame.image.load('img/player/Idle/0.png')
         # lets us scale our image
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
@@ -42,13 +43,18 @@ class Soldier(pygame.sprite.Sprite):
 
         if moving_left:
             dx = -self.speed
+            self.flip = True
+            self.direction = -1
         if moving_right:
             dx = self.speed
+            self.flip = False
+            self.direction = 1
         self.rect.x += dx
         self.rect.y += dy
 
     def draw(self):
-        screen.blit(self.image, self.rect)
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
 
 
 player = Soldier(200, 200, 3, 5)
