b2b0d316e4402aac05a8312e9bfeaf55bf2338ca
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-24 17:12:45 -0800

    "scaling added"

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ pygame.display.set_caption('Shooter')
 #draws player coordinates
 x = 200
 y = 200
+scale = 3
 img = pygame.image.load('img/player/Idle/0.png')
+#lets us scale our image
+img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
 #draws rect around imgage
 rect = img.get_rect()
 rect.center = (x, y)
