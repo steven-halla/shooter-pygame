424399a22f873aeb88442641aaa6ef13c7d3f68b
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-27 15:55:08 -0800

    "grendades now explode"

--- main.py
+++ main.py
@@ -216,21 +216,31 @@ class Grenade(pygame.sprite.Sprite):
         self.rect.x += dx
         self.rect.y += dy
 
+        self.timer -= 1
+        if self.timer <= 0:
+            self.kill()
+            explosion = Explosion(self.rect.x, self.rect.y, 0.5)
+            explosion_group.add(explosion)
+
 class Explosion(pygame.sprite.Sprite):
     def __init__(self, x, y, scale):
         pygame.sprite.Sprite.__init__(self)
         self.images = []
         for num in range(1, 6):
             img = pygame.image.load(f'img/explosion/exp{num}.png').convert_alpha()
-            img = pygame.transform.scale(int(img, (img.get_width() * scale), img.get_height() * scale))
-        self.image = grenade_img
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            self.images.append(img)
+        self.frame_index = 0
+        self.image = self.images[self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.counter = 0
 
 
 #sprite groups
 bullet_group = pygame.sprite.Group()
 grenade_group = pygame.sprite.Group()
+explosion_group = pygame.sprite.Group()
 
 
 player = Soldier('player', 200, 200, 3, 5, 20, 5)
@@ -256,8 +266,11 @@ while run:
     #update and draw
     bullet_group.update()
     grenade_group.update()
+    explosion_group.update()
+
     bullet_group.draw(screen)
     grenade_group.draw(screen)
+    explosion_group.draw(screen)
 
 
 
