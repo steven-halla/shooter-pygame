fe8b5d890c201beb01d9d206e9a0fdc296ddcab0
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-01-04 18:02:48 -0800

    "grenades are now thrown properly"

--- main.py
+++ main.py
@@ -396,22 +396,34 @@ class Grenade(pygame.sprite.Sprite):
         self.image = grenade_img
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
         self.direction = direction
 
     def update(self):
         self.vel_y += GRAVITY
         dx = self.direction * self.speed
         dy = self.vel_y
-        self.rect.x += dx
-        self.rect.y += dy
 
-        if self.rect.bottom + dy > 300:
-            dy = 300 - self.rect.bottom
-            self.speed = 0
+        #cehck collsion with envirnment
+        for tile in world.obstacle_list:
 
-        if self.rect.left + dx < 0 or self.rect.right + dx > SCREEN_WIDTH - 100:
-            self.direction *= -1
-            dx = self.direction * self.speed
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                self.direction *= -1
+                dx = self.direction * self.speed
+
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                # check if thrown up
+                self.speed = 0
+                if self.vel_y < 0:
+                    self.vel_y = 0
+                    dy = tile[1].bottom - self.rect.top
+                # check for falling
+                elif self.vel_y >= 0:
+                    self.vel_y = 0
+                    dy = tile[1].top - self.rect.bottom
+
+
 
         self.rect.x += dx
         self.rect.y += dy
