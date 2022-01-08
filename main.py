5728f059eae0c35fa12883f2eb570be12111f2b0
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-01-07 18:35:28 -0800

    "restart button pops up on death"

--- main.py
+++ main.py
@@ -669,6 +669,18 @@ while run:
             screen_scroll = 0
             if restart_button.draw(screen):
                 bg_scroll = 0
+                world_data = reset_level()
+                #load in level data and create world
+                with open(f'level{level}_data.csv', newline='') as csvfile:
+                    reader = csv.reader(csvfile, delimiter=',')
+                    for x, row in enumerate(reader):
+                        for y, tile in enumerate(row):
+                            world_data[x][y] = int(tile)
+                print(world_data)
+
+                # makes variables global
+                world = World()
+                player, health_bar = world.process_data(world_data)
 
 
 
